ac3374e661e55334e2582079aa192566c3f12544
Jayce Azua <jayce.azua@gmail.com>
2018-09-02 19:01:17 -0400

    setting up middleware

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
+// Template Engine setup
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// CRUD Resource >> Posts
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Reddit Clone is listening on port: ${PORT}`);
+
+
+app.listen(port, () => {
+  console.log(`Reddit Clone is listening on port: ${port}`);
 });
