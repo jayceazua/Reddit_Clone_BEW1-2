7b82000d1293637360b56cad49d61429d96dab97
Jayce Azua <jayce.azua@gmail.com>
2018-09-03 14:28:14 -0400

    added edit endpoint

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,8 +7,18 @@ router.get('/new', (req, res, next) => {
   res.render('posts/new');
 });
 
+// READ a.k.a. SHOW
+router.get('/:id', (req, res, next) => {
+  // LOOK UP THE POST
+  Post.findById(req.params.id).then((post) => {
+    res.render('posts/show', { post });
+  }).catch((err) => {
+    console.log(err.message);
+  });
+});
+
 // CREATE
-router.post('/new', (req, res, next) => {
+router.post('', (req, res, next) => {
   // INSTANTIATE INSTANCE OF POST MODEL
   let post = new Post(req.body);
   // SAVE INSTANCE OF POST MODEL TO DB
@@ -21,18 +31,21 @@ router.post('/new', (req, res, next) => {
   });
 });
 
-// READ a.k.a. SHOW
-router.get('/:id', (req, res, next) => {
-  // LOOK UP THE POST
+// UPDATE
+router.get('/:id/edit', (req, res, next) => {
   Post.findById(req.params.id).then((post) => {
-    res.render('posts/show', { post });
+    res.render('posts/edit', { post });
   }).catch((err) => {
     console.log(err.message);
   });
 });
 
-// UPDATE
-
+router.put('/:id', (req, res, next) => {
+  console.log('Post was updated.');
+});
 // DELETE
+router.delete('/:id', (req, res, next) => {
+  console.log('Post deleted.');
+});
 
 module.exports = router
