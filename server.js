48912115a22452ebbb656f727b5a16f77ada8172
Jayce Azua <jayce.azua@gmail.com>
2018-09-02 20:21:01 -0400

    added templare engine

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Template Engine setup
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/views/layouts/'
+}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Body Parser
