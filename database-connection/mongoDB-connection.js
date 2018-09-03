b31b3f6abfa910cda2f07eaa2b35048673a405f1
Jayce Azua <jayce.azua@gmail.com>
2018-09-02 23:21:32 -0400

    mongo database connected.

--- database-connection/mongoDB-connection.js
+++ database-connection/mongoDB-connection.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const db = mongoose.connection;
+mongoose.Promise = global.Promise;
+// mLab dbuser
+const dbuser = process.env.dbuser;
+// mLab dbpassword
+const dbpassword = process.env.dbpassword;
+// mLab API KEY
+const mLabKEY = process.env.mLabKEY;
 
 // MongoDB URI - could be in a config file
-let dbUri = process.env.MONGODB_URI || `mongodb://${dbuser}:${dbpassword}@ds141952.mlab.com:41952/reddit_clone_bew1-2`;
+let dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/reddit_clone_bew1-1';
 
-mongoose.Promise = global.Promise;
-mongoose.connect(dbUri);
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', () => {console.log('Mongo Database connected!')});
+mongoose.connect(dbURI, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {console.log('Reddit Clone BEW 1-2 MongoDB Connected.')});
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
+mongoose.set('debug', true);
