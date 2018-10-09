bbf61def53f7a23260b75634130a8df7e815cc43
Jayce Azua <jayce.azua@gmail.com>
2018-10-09 13:52:37 -0700

    I will do test later...

--- test/posts.js
+++ test/posts.js
@@ -6,23 +6,11 @@ const Post = require('../models/post')
 chai.use(chaiHttp);
 
 describe('Testing posts routes:', () => {
-
-  it('Should create with valid attributes at POST /posts', (done) => {
-    chai.request('http://localhost:3000')
-    .get('/')
-    .then((res) => {
-      res.status.should.equal(200);
-      return done();
-    }).catch((err) => {
-      return done(err);
-    });
-  });
-
-  it('Should create a new post', (done) => {
-    // How many posts are there now?
-    // Make a request to create another
-    // Check that the database has one more post in it
-    // Check that the response is a successful
-  });
+  // Should create with valid attributes at POST /posts
+  // Should create a new post
+  // How many posts are there now?
+  // Make a request to create another
+  // Check that the database has one more post in it
+  // Check that the response is a successful
 
 });
