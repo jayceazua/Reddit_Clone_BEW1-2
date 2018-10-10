5e8cd0a012dfddeab9ce3c4252b11a168a8983fd
Jayce Azua <jayce.azua@gmail.com>
2018-10-09 21:38:58 -0700

    finishing reddit...

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,9 +12,11 @@ router.post('/sign-up', (req, res) => {
     // Create User
     const user = new User(req.body);
     user.save().then((user) => {
+        let token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
         res.redirect('/');
     }).catch((err) => {
         console.log(err.message);
+        return res.status(400).send({ err: err });
     });
 });
 
